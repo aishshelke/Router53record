e39ec04b1dc56794997199320f7b480c11ffb209
Aishwarya Shelke <45987899+aishshelke@users.noreply.github.com>
2022-12-18 21:04:21 +0530

    Update createrecord.tf

--- createrecord.tf
+++ createrecord.tf
@@ -1,23 +1,11 @@
-resource "aws_elb" "main" {
-  name               = "foobar-terraform-elb"
-  availability_zones = ["us-east-1c"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-}
-
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "example.com"
+  zone_id = abc.io.primary.Z0427719376SIPEUTQP9C
+  name    = "$REPOSITORY_NAME_$BITBUCKET_SOURCE_BRANCH"
   type    = "A"
 
   alias {
-    name                   = aws_elb.main.dns_name
-    zone_id                = aws_elb.main.zone_id
+    name                   = dev-alb.main.abc.io
+    zone_id                = dev-alb.main.Z0427719376SIPEUTQP9C
     evaluate_target_health = true
   }
 }
